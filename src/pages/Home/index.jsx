27280e601fe7b835516f05a8e820ed803442e50e
wxzhishang <15082781523@163.com>
2024-06-23 22:38:13 +0800

    headers and logo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Header from "../../components/Header";
 
 function Home() {
     return (
-        <div>123</div>
+        <>
+            <Header></Header>
+        </>
     )
 }
 
