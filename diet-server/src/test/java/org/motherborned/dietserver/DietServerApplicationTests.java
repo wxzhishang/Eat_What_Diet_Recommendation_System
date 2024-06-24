b147a593780002f932ee161f00ddc72cea7e3989
SaberYoung <saberyoung666@gmail.com>
2024-06-24 17:11:15 +0800

    配置后端环境

---
+++ diet-server/src/test/java/org/motherborned/dietserver/DietServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.motherborned.dietserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
